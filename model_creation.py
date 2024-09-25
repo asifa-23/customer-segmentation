7828cc0ececec1aff9eccaae482bcc614b28eed1
ASIFA BEGUM D <134198210+asifa-23@users.noreply.github.com>
2024-09-25 11:10:38 +0530

    Create model_creation.py

---
+++ model_creation.py
@@ -0,0 +1,12 @@
+# Create the K means model for different values of K
+def try_different_clusters(K, data):
+
+    cluster_values = list(range(1, K+1))
+    inertias=[]
+
+    for c in cluster_values:
+        model = KMeans(n_clusters = c,init='k-means++',max_iter=400,random_state=42)
+        model.fit(data)
+        inertias.append(model.inertia_)
+
+    return inertias
